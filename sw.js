8e96dded270e3fe08652d768c9116a1a4dc1d6ac
geacam <sgonzalezgeacam@gmail.com>
2026-05-25 08:38:07 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Este código registra el evento fetch obligatorio para que el móvil lo detecte como PWA
+self.addEventListener('fetch', (event) => {
+    // Puedes dejarlo vacío o usarlo más adelante para almacenamiento en caché
+});
