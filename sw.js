02b8827adabb86093111d5f7976522e03bfee631
geacam <sgonzalezgeacam@gmail.com>
2026-05-25 13:10:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'geomaps-v1';
+const CACHE = 'geomaps-v1.1';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
